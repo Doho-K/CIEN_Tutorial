e42d02bf852ede58c31a4735cff8232206548fb0
Doho-K <kimdohwa2@naver.com>
2023-07-12 03:05:06 +0900

    Asset Add&Basic stuffs

---
+++ CIEN_Tutorial/Assets/Codes/DinoEnemy.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DinoEnemy : MonoBehaviour
+{
+    [SerializeField] private float leftCap;
+    [SerializeField] private float rightCap;
+
+    [SerializeField] private float jumpLength = 10f;
+    [SerializeField] private float jumpHeight = 15f;
+    [SerializeField] private LayerMask ground;
+
+    private bool facingLeft = true;
+    private Collider2D coll;
+    private Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        coll = GetComponent<Collider2D>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if (facingLeft)
+        {
+            if(transform.position.x> leftCap)
+            {
+
+                if(transform.localScale.x != 1)
+                {
+                    transform.localScale = new Vector3(1, 1);
+                }
+                if (coll.IsTouchingLayers(ground))
+                {
+                    rb.velocity = new Vector2(-jumpLength, jumpHeight);
+                }
+        }
+            else
+            {
+                facingLeft = false;
+            }
+        }
+        else
+        {
+            if (transform.position.x < rightCap)
+            {
+
+                if (transform.localScale.x != -1)
+                {
+                    transform.localScale = new Vector3(-1, 1);
+                }
+                if (coll.IsTouchingLayers(ground))
+                {
+                    rb.velocity = new Vector2(jumpLength, jumpHeight);
+                }
+            }
+            else
+            {
+                facingLeft = true;
+            }
+        }
+        
+    }
+}
