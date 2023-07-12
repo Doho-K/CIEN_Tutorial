7edae6a9baa570ba61e8434a41fe557b32bde181
Doho-K <kimdohwa2@naver.com>
2023-07-12 21:40:29 +0900

    Prefab(Enemy Dino)Added

--- CIEN_Tutorial/Assets/Codes/PlayerController.cs
+++ CIEN_Tutorial/Assets/Codes/PlayerController.cs
@@ -10,6 +10,7 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        rb = GetComponent<Rigidbody2D>();
         
     }
 
