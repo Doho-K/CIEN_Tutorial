10207096a66d7cd8e87cc81131fe2b3be4bf8d10
Dedo <kdh51050@naver.com>
2023-07-13 11:47:34 +0900

    Add PlayerController

--- CIEN_Tutorial/Assets/Codes/PlayerController.cs
+++ CIEN_Tutorial/Assets/Codes/PlayerController.cs
@@ -7,6 +7,8 @@ public class PlayerController : MonoBehaviour
     private Rigidbody2D rb;
     private Animator anim;
     private Collider2D coll;
+    public float maxSpeed;
+    public float jumpPower;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,7 +19,11 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //스페이스바로 점프
+        if (Input.GetKeyDown(KeyCode.Space) && this.rb.velocity.y == 0)
+        {
+            rb.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
+        }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -25,6 +31,23 @@ public class PlayerController : MonoBehaviour
         if(collision.gameObject.tag == "Enemy")//적에게 피격시
         {
 
+        }
+    }
+
+    void FixedUpdate()
+    {
+        //방향키로 이동
+        float h = Input.GetAxisRaw("Horizontal");
+
+        rb.AddForce(Vector2.right * h, ForceMode2D.Impulse);
+
+        if (rb.velocity.x > maxSpeed)
+        {
+            rb.velocity = new Vector2(maxSpeed, rb.velocity.y);
+        }
+        else if (rb.velocity.x < maxSpeed * (-1))
+        {
+            rb.velocity = new Vector2(maxSpeed * (-1), rb.velocity.y);
         }
     }
 }
