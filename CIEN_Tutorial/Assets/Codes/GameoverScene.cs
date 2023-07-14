4a44bed790b7d06b8adf12bf8bdde4d5a007468a
Dedo <kdh51050@naver.com>
2023-07-14 19:59:22 +0900

    Add GameoverScene

---
+++ CIEN_Tutorial/Assets/Codes/GameoverScene.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameoverScene : MonoBehaviour
+{
+    public void GameSceneCtrl()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
