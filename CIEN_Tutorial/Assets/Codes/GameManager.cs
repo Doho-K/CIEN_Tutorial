89b4eb904d87b5e17d359d9aa693adcf921d07ea
Doho-K <kimdohwa2@naver.com>
2023-07-18 01:50:22 +0900

    Death&Basic System almost done

---
+++ CIEN_Tutorial/Assets/Codes/GameManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager instance;
+    [SerializeField] private GameObject text;
+    public int health = 5;
+    public static GameManager Instance
+    {
+        get
+        {
+            if (!instance)
+            {
+                instance = FindObjectOfType(typeof(GameManager)) as GameManager;
+
+                
+            }
+            return instance;
+        }
+    }
+
+
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+        else if(instance != this)
+        {
+            Destroy(gameObject);
+        }
+        DontDestroyOnLoad(gameObject);
+    }
+    
+    void Start()
+    {
+
+
+    }
+    private void Update()
+    {
+        if(text == null)
+        {
+            text = GameObject.Find("HealthText");
+        }
+        text.GetComponent<TextMeshProUGUI>().text = health.ToString();
+    }
+    // Start is called before the first frame update
+
+}
--- CIEN_Tutorial/Assets/Codes/PlayerController.cs
+++ CIEN_Tutorial/Assets/Codes/PlayerController.cs
@@ -8,23 +8,46 @@ public class PlayerController : MonoBehaviour
     private Rigidbody2D rb;
     private Animator anim;
     private Collider2D coll;
-    
+
+    private bool IsDead = false;
+    private enum State { idle,running,jumping,falling}
+    private State state = State.idle;
+    [SerializeField] private GameManager gm;
     [SerializeField] private float maxSpeed;
     [SerializeField] private float jumpPower;
     [SerializeField] private LayerMask ground;
     // Start is called before the first frame update
     void Start()
     {
+        gm = GameManager.FindObjectOfType<GameManager>();
+        anim = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
         coll = GetComponent<Collider2D>();
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if(collision.gameObject.tag == "Enemy" || collision.gameObject.tag == "Floor")//적에게 피격시
+        if(collision.gameObject.tag == "Enemy")//적에게 피격시
         {
-            Destroy(gameObject);
-            SceneManager.LoadScene("GameoverScene");
+            if(state == State.falling)
+            {
+                Jump();
+                Destroy(collision.gameObject);
+            }
+            else
+            {
+                Death();
+            
+            }
+            
+        }
+        else if(collision.gameObject.tag == "Floor")
+        {
+            anim.SetBool("IsDead", true);
+            rb.velocity = new Vector2(0, 40);
+            Invoke("Death", 0.5f);
+    
+
         }
     }
 
@@ -32,22 +55,84 @@ public class PlayerController : MonoBehaviour
     {
         //방향키로 이동
         float h = Input.GetAxis("Horizontal");
-
+        if (Input.GetButtonUp("Horizontal"))
+        {
+            rb.velocity = new Vector2(rb.velocity.normalized.x * 0.5f, rb.velocity.y);
+        }
         if (h < 0)
         {
+           
             rb.velocity = new Vector2(-1 * maxSpeed, rb.velocity.y);
             transform.localScale = new Vector2(-1, 1);
         }
         else if(h > 0)
         {
+
             rb.velocity = new Vector2( maxSpeed, rb.velocity.y);
             transform.localScale = new Vector2(1, 1);
         }
 
-        if (Input.GetButtonDown("Jump") && coll.IsTouchingLayers(ground))
+        else
         {
-            rb.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
+
+            
         }
 
+        if (Input.GetButtonDown("Jump") && coll.IsTouchingLayers(ground))
+        {
+            Jump();
+        }
+
+        VelocityState();
+        anim.SetInteger("state", (int)state);
+
+    }
+
+    private void Jump()
+    {
+            rb.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
+            state = State.jumping;
+    }
+    private void VelocityState()
+    {
+        if (state == State.jumping)
+        {
+            if(rb.velocity.y < .1f)
+            {
+                state = State.falling;
+            }
+        }
+        else if(state == State.falling)
+        {
+            if (coll.IsTouchingLayers(ground))
+            {
+                state = State.idle;
+            }
+        }
+        else if (Mathf.Abs(rb.velocity.x) > 2f)
+        {
+            state = State.running;
+        }
+        else
+        {
+            state = State.idle;
+        }
+    }
+
+    
+
+    private void Death()
+    {
+        gm.health -= 1;
+        rb.bodyType = RigidbodyType2D.Static;
+        IsDead = true;
+        anim.SetBool("IsDead", IsDead);
+        Invoke("MoveScene", 2);
+    }
+
+    private void MoveScene()
+    {
+        Destroy(gameObject);
+        SceneManager.LoadScene("GameoverScene");
     }
 }
